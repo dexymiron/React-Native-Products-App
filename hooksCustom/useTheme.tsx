e672b8ac7bae5cde466842550fdabb419468cb25
dexymiron <dexymiron@gmail.com>
2025-02-04 18:01:08 +0200

    6: added check to useTheme -useTheme must use only in ThemeProvider

--- hooksCustom/useTheme.tsx
+++ hooksCustom/useTheme.tsx
@@ -1,4 +1,10 @@
 import { useContext } from "react";
 import { ThemeContext } from "../ContextAPI/ThemeContext/ThemeContext";
 
-export const useTheme = () => useContext(ThemeContext);
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must used in ThemeProvider");
+  }
+  return context;
+};
